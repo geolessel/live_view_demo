13856b36a1761a8382fc9b97b3d2c3dca8cccc1c
Geoffrey Lessel <geo@geoffreylessel.com>
2019-09-28 19:07:23 -0700

    set up for gigalixir

--- config/prod.exs
+++ config/prod.exs
@@ -16,6 +16,16 @@ config :live_view_demo, LiveViewDemoWeb.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
+config :live_view_demo, LiveViewDemoWeb.Endpoint,
+  load_from_system_env: true,
+  # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
+  http: [port: {:system, "PORT"}],
+  # Without this line, your app will not start the web server!
+  server: true,
+  secret_key_base: "${SECRET_KEY_BASE}",
+  url: [host: "${APP_NAME}.gigalixirapp.com", port: 443],
+  cache_static_manifest: "priv/static/cache_manifest.json"
+
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
