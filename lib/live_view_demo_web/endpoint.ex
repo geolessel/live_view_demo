12fc851ad75df21d891db3b65ab6257f6c6d265d
Geoffrey Lessel <geo@geoffreylessel.com>
2019-09-30 16:07:54 -0500

    Force HTTPS

--- lib/live_view_demo_web/endpoint.ex
+++ lib/live_view_demo_web/endpoint.ex
@@ -3,7 +3,13 @@ defmodule LiveViewDemoWeb.Endpoint do
 
   socket "/socket", LiveViewDemoWeb.UserSocket,
     websocket: true,
-    longpoll: false
+    longpoll: false,
+    check_origin: [
+      "localhost",
+      "127.0.0.1",
+      "https://elixirregex.com",
+      "https://offbeat-adventurous-mussaurus.gigalixirapp.com"
+    ]
 
   socket "/live", Phoenix.LiveView.Socket
 
