8909dbc4a1b5ab8fb7b2d7b445861e0e54f68c2a
Geoffrey Lessel <geo@geoffreylessel.com>
2019-10-01 17:01:38 -0500

    Live update the URL! Allow linking to fully-filled out forms.

--- lib/live_view_demo_web/live/page_live.ex
+++ lib/live_view_demo_web/live/page_live.ex
@@ -12,25 +12,26 @@ defmodule LiveViewDemoWeb.PageLive do
        pattern: "",
        input: "",
        error: "",
-       options: []
+       modifiers: []
      })}
   end
 
-  def handle_event("form_update", %{"regex" => %{"pattern" => ""}}, socket),
-    do: {:noreply, assign(socket, compiled: nil)}
+  def handle_params(params, _url, socket) when map_size(params) == 0 do
+    {:noreply, assign(socket, compiled: nil)}
+  end
 
-  def handle_event("form_update", %{"regex" => params}, socket) do
-    pattern = Map.get(params, "pattern")
-    input = Map.get(params, "input")
-    options = Map.get(params, "options", [])
+  def handle_params(params, _url, socket) do
+    input = Map.get(params, "input", "")
+    pattern = Map.get(params, "pattern", "")
+    modifiers = Map.get(params, "modifiers", "") |> String.split("")
 
-    case Regex.compile(pattern, Enum.join(options)) do
+    case Regex.compile(pattern, Enum.join(modifiers)) do
       {:ok, compiled} ->
         {:noreply,
          assign(socket, %{
            compiled: compiled,
            pattern: pattern,
-           options: options,
+           modifiers: modifiers,
            input: input,
            error: ""
          })}
@@ -40,10 +41,36 @@ defmodule LiveViewDemoWeb.PageLive do
          assign(socket, %{
            compiled: nil,
            pattern: pattern,
-           options: options,
+           modifiers: modifiers,
            input: input,
            error: error
          })}
+    end
+  end
+
+  def handle_event("form_update", %{"regex" => %{"pattern" => ""}}, socket),
+    do: {:noreply, live_redirect(assign(socket, compiled: nil), to: "/")}
+
+  def handle_event("form_update", %{"regex" => params}, socket) do
+    query =
+      ["pattern", "input", "modifiers"]
+      |> Enum.reduce(%{}, fn str, acc ->
+        case Map.get(params, str) do
+          nil -> acc
+          list when is_list(list) -> Map.put(acc, str, Enum.join(list))
+          "" -> acc
+          val -> Map.put(acc, str, val)
+        end
+      end)
+      |> Enum.reverse()
+      |> URI.encode_query()
+
+    case String.length(query) do
+      0 ->
+        {:noreply, live_redirect(socket, to: "/")}
+
+      _ ->
+        {:noreply, live_redirect(socket, to: "/?#{query}")}
     end
   end
 end
